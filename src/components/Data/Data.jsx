ed57dd2f9a96af1fed737ea64bf8966b666ae931
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-11-23 07:28:18 -0800

    chages favorite coponent

--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -1,7 +1,7 @@
 export const DataForm =[
     { user: Math.floor(Math.random() * 1000), fullname: "mohammad lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com' ,favorite: false, },
-    { user: Math.floor(Math.random() * 1000), fullname: "allli lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com' ,favorite: false,},
-    { user: Math.floor(Math.random() * 1000), fullname: "maneli lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com',favorite:true, },
-    { user: Math.floor(Math.random() * 1000), fullname: "milad lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com' ,favorite: true,},
-    { user: Math.floor(Math.random() * 1000), fullname: "sara lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com' ,favorite: true,},
+    { user: Math.floor(Math.random() * 1000), fullname: "allli lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com' ,favorite: "favorite",},
+    { user: Math.floor(Math.random() * 1000), fullname: "maneli lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com',favorite:"favorite" },
+    { user: Math.floor(Math.random() * 1000), fullname: "milad lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com' ,favorite: "unFavorite"},
+    { user: Math.floor(Math.random() * 1000), fullname: "sara lavasani",  phone: "09024914059",  City: "mashhad", email: 'fatemehlavasani141@gmail.com' ,favorite: "favorite"},
 ]
